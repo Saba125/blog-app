dda9615525f4e194d99c4a5acdd0fa10999bcded
Saba125 <jiadzeb@gmail.com>
2024-01-12 11:42:53 +0400

    added theme-toggle

--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -1,6 +1,7 @@
 import Container from "../Container";
 import Logo from "./Logo";
 import SocialLinks from "./SocialLinks";
+import { ModeToggle } from "./ThemeToggle";
 import UserMenu from "./UserMenu";
 
 const Navbar = () => {
@@ -11,7 +12,17 @@ const Navbar = () => {
           <div className="flex items-center justify-between">
             <SocialLinks />
             <Logo />
-            <UserMenu />
+            <div
+              className="
+            flex
+            flex-row
+            items-center
+            gap-3
+            "
+            >
+              <ModeToggle />
+              <UserMenu />
+            </div>
           </div>
         </div>
       </Container>
