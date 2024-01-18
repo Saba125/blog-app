5283b0f19016731c7b5196cce37efbc157578c75
Saba125 <jiadzeb@gmail.com>
2024-01-18 15:25:40 +0400

    auth-fix

--- app/view-blog/page.tsx
+++ app/view-blog/page.tsx
@@ -1,5 +1,5 @@
 import { getServerSession } from "next-auth";
-import { authOptions } from "../api/auth/[...nextauth]/route";
+import { authOptions } from "@/lib/authOptions";
 import prisma from "@/lib/prisma";
 import Container from "@/components/Container";
 import SinglePost from "@/components/SinglePost";
