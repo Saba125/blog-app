0723dcebd0b5daa8a9293b1a58eb9b77a07f8a90
Saba125 <jiadzeb@gmail.com>
2024-01-18 16:08:09 +0400

    plz work

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,13 +1,15 @@
-import { PrismaClient } from "@prisma/client/edge";
+import { PrismaClient } from "@prisma/client";
 
-let prisma: PrismaClient;
+const prismaClientSingleton = () => {
+  return new PrismaClient();
+};
 
 declare global {
-  var prisma: PrismaClient;
+  var prisma: undefined | ReturnType<typeof prismaClientSingleton>;
 }
 
-if (!global.prisma) {
-  global.prisma = new PrismaClient();
-}
-prisma = global.prisma;
+const prisma = globalThis.prisma ?? prismaClientSingleton();
+
 export default prisma;
+
+if (process.env.NODE_ENV !== "production") globalThis.prisma = prisma;
