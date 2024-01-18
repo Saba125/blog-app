d5a37f9aafd2b1745a9f9ee49ebe6e18eb047728
Saba125 <jiadzeb@gmail.com>
2024-01-18 15:50:00 +0400

    route changes

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,12 +1,5 @@
-import NextAuth, { AuthOptions } from "next-auth";
-import CredentialsProvider from "next-auth/providers/credentials";
-import GithubProvider from "next-auth/providers/github";
-import GoogleProvider from "next-auth/providers/google";
-import { PrismaAdapter } from "@auth/prisma-adapter";
-import bcrypt from "bcrypt";
-import prisma from "@/lib/prisma";
-import { Adapter } from "next-auth/adapters";
 import { authOptions } from "@/lib/authOptions";
+import NextAuth from "next-auth";
 
 const handler = NextAuth(authOptions);
 export { handler as GET, handler as POST };
